3394057eb6202e858e4cb583b5e10a01d4a0544c
lechibang-1512 <banglechi.work@gmail.com>
2026-09-03 23:28:11 +0700

    Organize Playground test mission ships alphabetically with Onyx as flagship

--- src/data/missions/SKR_playground/MissionDefinition.java
+++ src/data/missions/SKR_playground/MissionDefinition.java
@@ -24,59 +24,50 @@ public class MissionDefinition implements MissionDefinitionPlugin {
         // "Tactical Objectives" on the mission detail screen
         api.addBriefingItem("Let's do SCIENCE! Every unique hull in SEEKER is ready for testing.");
 
-        // Set up the player's fleet (One unique hull each)
-        // Frigates & Gunships
-        api.addToFleet(FleetSide.PLAYER, "SKR_malet_standard", FleetMemberType.SHIP, false);
-        api.addToFleet(FleetSide.PLAYER, "SKR_endymion_standard", FleetMemberType.SHIP, false);
-        api.addToFleet(FleetSide.PLAYER, "SKR_augur_standard", FleetMemberType.SHIP, false);
-        api.addToFleet(FleetSide.PLAYER, "SKR_follower_standard", FleetMemberType.SHIP, false);
-        api.addToFleet(FleetSide.PLAYER, "SKR_believer_standard", FleetMemberType.SHIP, false);
-
-        // Destroyers
-        api.addToFleet(FleetSide.PLAYER, "SKR_adze_standard", FleetMemberType.SHIP, false);
-        api.addToFleet(FleetSide.PLAYER, "SKR_marksman_combat", FleetMemberType.SHIP, false);
-        api.addToFleet(FleetSide.PLAYER, "SKR_butterfly_standard", FleetMemberType.SHIP, false);
-        api.addToFleet(FleetSide.PLAYER, "SKR_aethernium_standard", FleetMemberType.SHIP, false);
-        api.addToFleet(FleetSide.PLAYER, "SKR_557_standard", FleetMemberType.SHIP, false);
-        api.addToFleet(FleetSide.PLAYER, "SKR_obelisk_arch", FleetMemberType.SHIP, false);
-        api.addToFleet(FleetSide.PLAYER, "SKR_cultist_standard", FleetMemberType.SHIP, false);
-        api.addToFleet(FleetSide.PLAYER, "SKR_fanatic_standard", FleetMemberType.SHIP, false);
-        api.addToFleet(FleetSide.PLAYER, "SKR_doctrinaire_standard", FleetMemberType.SHIP, false);
-
-        // Cruisers & Combat Yachts
-        api.addToFleet(FleetSide.PLAYER, "SKR_halligan_standard", FleetMemberType.SHIP, false);
-        api.addToFleet(FleetSide.PLAYER, "SKR_hedone_deluxe", FleetMemberType.SHIP, false);
-        api.addToFleet(FleetSide.PLAYER, "SKR_bullhorn_standard", FleetMemberType.SHIP, false);
-        api.addToFleet(FleetSide.PLAYER, "SKR_poisonivy_standard", FleetMemberType.SHIP, false);
-        api.addToFleet(FleetSide.PLAYER, "SKR_tumbleweed_standard", FleetMemberType.SHIP, false);
-        api.addToFleet(FleetSide.PLAYER, "SKR_clipper_advanced", FleetMemberType.SHIP, false);
-        api.addToFleet(FleetSide.PLAYER, "SKR_retiarius_standard", FleetMemberType.SHIP, false);
-        api.addToFleet(FleetSide.PLAYER, "SKR_fresnel_standard", FleetMemberType.SHIP, false);
-        api.addToFleet(FleetSide.PLAYER, "SKR_trailblazer_assault", FleetMemberType.SHIP, false);
-        api.addToFleet(FleetSide.PLAYER, "SKR_balisong_overdrive", FleetMemberType.SHIP, false);
-        api.addToFleet(FleetSide.PLAYER, "SKR_zealot_standard", FleetMemberType.SHIP, false);
-        api.addToFleet(FleetSide.PLAYER, "SKR_guru_standard", FleetMemberType.SHIP, false);
-        api.addToFleet(FleetSide.PLAYER, "SKR_devotee_standard", FleetMemberType.SHIP, false);
-
-        // Battleships, Dreadnoughts & Bosses
-        api.addToFleet(FleetSide.PLAYER, "SKR_cassowary_standard", FleetMemberType.SHIP, false);
-        api.addToFleet(FleetSide.PLAYER, "SKR_cassowaryRH_assault", FleetMemberType.SHIP, false);
-        api.addToFleet(FleetSide.PLAYER, "SKR_quicksilver_deepDive", FleetMemberType.SHIP, false);
-        api.addToFleet(FleetSide.PLAYER, "SKR_dawn_moonlight", FleetMemberType.SHIP, false);
-        api.addToFleet(FleetSide.PLAYER, "SKR_onyx_newMoon", FleetMemberType.SHIP, false);
-        api.addToFleet(FleetSide.PLAYER, "SKR_voulge_standard", FleetMemberType.SHIP, false);
-        api.addToFleet(FleetSide.PLAYER, "SKR_gawon_avatar", FleetMemberType.SHIP, false);
-        api.addToFleet(FleetSide.PLAYER, "SKR_siegfried_base", FleetMemberType.SHIP, false);
-        api.addToFleet(FleetSide.PLAYER, "SKR_nova_apex", FleetMemberType.SHIP, false);
-        api.addToFleet(FleetSide.PLAYER, "SKR_keep_safeguard", FleetMemberType.SHIP, false);
-        api.addToFleet(FleetSide.PLAYER, "SKR_rampage_01", FleetMemberType.SHIP, false);
-        api.addToFleet(FleetSide.PLAYER, "SKR_whiteDwarf_1", FleetMemberType.SHIP, false);
-        api.addToFleet(FleetSide.PLAYER, "SKR_cataclysm_1", FleetMemberType.SHIP, false);
-
-        // Special Civilians
-        api.addToFleet(FleetSide.PLAYER, "CIV_blitzkrieg_standard", FleetMemberType.SHIP, "SAS Blitzkrieg", false);
-        api.addToFleet(FleetSide.PLAYER, "CIV_demeter_standard", FleetMemberType.SHIP, "SAS Demeter", false);
-        api.addToFleet(FleetSide.PLAYER, "CIV_titanic_standard", FleetMemberType.SHIP, "SAS Titanic VII", false);
+        // Set up the player's fleet (Alphabetical by ship name, Onyx flagship)
+        api.addToFleet(FleetSide.PLAYER, "SKR_onyx_newMoon", FleetMemberType.SHIP, true); // Flagship: Onyx
+        api.addToFleet(FleetSide.PLAYER, "SKR_557_standard", FleetMemberType.SHIP, false); // 557
+        api.addToFleet(FleetSide.PLAYER, "SKR_adze_standard", FleetMemberType.SHIP, false); // Adze
+        api.addToFleet(FleetSide.PLAYER, "SKR_aethernium_standard", FleetMemberType.SHIP, false); // Aethernium
+        api.addToFleet(FleetSide.PLAYER, "SKR_augur_standard", FleetMemberType.SHIP, false); // Augur
+        api.addToFleet(FleetSide.PLAYER, "SKR_balisong_overdrive", FleetMemberType.SHIP, false); // Balisong
+        api.addToFleet(FleetSide.PLAYER, "SKR_believer_standard", FleetMemberType.SHIP, false); // Believer
+        api.addToFleet(FleetSide.PLAYER, "CIV_blitzkrieg_standard", FleetMemberType.SHIP, "SAS Blitzkrieg", false); // Blitzkrieg
+        api.addToFleet(FleetSide.PLAYER, "SKR_bullhorn_standard", FleetMemberType.SHIP, false); // Bullhorn
+        api.addToFleet(FleetSide.PLAYER, "SKR_butterfly_standard", FleetMemberType.SHIP, false); // Butterfly
+        api.addToFleet(FleetSide.PLAYER, "SKR_cassowary_standard", FleetMemberType.SHIP, false); // Cassowary
+        api.addToFleet(FleetSide.PLAYER, "SKR_cataclysm_1", FleetMemberType.SHIP, false); // Cataclysm
+        api.addToFleet(FleetSide.PLAYER, "SKR_clipper_advanced", FleetMemberType.SHIP, false); // Clipper
+        api.addToFleet(FleetSide.PLAYER, "SKR_cultist_standard", FleetMemberType.SHIP, false); // Cultist
+        api.addToFleet(FleetSide.PLAYER, "SKR_dawn_moonlight", FleetMemberType.SHIP, false); // Dawn
+        api.addToFleet(FleetSide.PLAYER, "CIV_demeter_standard", FleetMemberType.SHIP, "SAS Demeter", false); // Demeter
+        api.addToFleet(FleetSide.PLAYER, "SKR_devotee_standard", FleetMemberType.SHIP, false); // Devotee
+        api.addToFleet(FleetSide.PLAYER, "SKR_doctrinaire_standard", FleetMemberType.SHIP, false); // Doctrinaire
+        api.addToFleet(FleetSide.PLAYER, "SKR_endymion_standard", FleetMemberType.SHIP, false); // Endymion
+        api.addToFleet(FleetSide.PLAYER, "SKR_fanatic_standard", FleetMemberType.SHIP, false); // Fanatic
+        api.addToFleet(FleetSide.PLAYER, "SKR_follower_standard", FleetMemberType.SHIP, false); // Follower
+        api.addToFleet(FleetSide.PLAYER, "SKR_fresnel_standard", FleetMemberType.SHIP, false); // Fresnel
+        api.addToFleet(FleetSide.PLAYER, "SKR_gawon_avatar", FleetMemberType.SHIP, false); // Gawon
+        api.addToFleet(FleetSide.PLAYER, "SKR_guru_standard", FleetMemberType.SHIP, false); // Guru
+        api.addToFleet(FleetSide.PLAYER, "SKR_halligan_standard", FleetMemberType.SHIP, false); // Halligan
+        api.addToFleet(FleetSide.PLAYER, "SKR_hedone_deluxe", FleetMemberType.SHIP, false); // Hedone
+        api.addToFleet(FleetSide.PLAYER, "SKR_malet_standard", FleetMemberType.SHIP, false); // Malet
+        api.addToFleet(FleetSide.PLAYER, "SKR_marksman_combat", FleetMemberType.SHIP, false); // Marksman
+        api.addToFleet(FleetSide.PLAYER, "SKR_nova_apex", FleetMemberType.SHIP, false); // Nova
+        api.addToFleet(FleetSide.PLAYER, "SKR_obelisk_arch", FleetMemberType.SHIP, false); // Obelisk
+        api.addToFleet(FleetSide.PLAYER, "SKR_poisonivy_standard", FleetMemberType.SHIP, false); // Poison Ivy
+        api.addToFleet(FleetSide.PLAYER, "SKR_quicksilver_deepDive", FleetMemberType.SHIP, false); // Quicksilver
+        api.addToFleet(FleetSide.PLAYER, "SKR_rampage_01", FleetMemberType.SHIP, false); // Rampage
+        api.addToFleet(FleetSide.PLAYER, "SKR_cassowaryRH_assault", FleetMemberType.SHIP, false); // Red Hand
+        api.addToFleet(FleetSide.PLAYER, "SKR_retiarius_standard", FleetMemberType.SHIP, false); // Retiarius
+        api.addToFleet(FleetSide.PLAYER, "SKR_keep_safeguard", FleetMemberType.SHIP, false); // Safeguard
+        api.addToFleet(FleetSide.PLAYER, "SKR_siegfried_base", FleetMemberType.SHIP, false); // Siegfried
+        api.addToFleet(FleetSide.PLAYER, "CIV_titanic_standard", FleetMemberType.SHIP, "SAS Titanic VII", false); // Titanic
+        api.addToFleet(FleetSide.PLAYER, "SKR_trailblazer_assault", FleetMemberType.SHIP, false); // Trailblazer
+        api.addToFleet(FleetSide.PLAYER, "SKR_tumbleweed_standard", FleetMemberType.SHIP, false); // Tumbleweed
+        api.addToFleet(FleetSide.PLAYER, "SKR_voulge_standard", FleetMemberType.SHIP, false); // Voulge
+        api.addToFleet(FleetSide.PLAYER, "SKR_whiteDwarf_1", FleetMemberType.SHIP, false); // White Dwarf
+        api.addToFleet(FleetSide.PLAYER, "SKR_zealot_standard", FleetMemberType.SHIP, false); // Zealot
 
         // Mark a ship as essential, if you want
         //api.defeatOnShipLoss("ISS Black Star");
